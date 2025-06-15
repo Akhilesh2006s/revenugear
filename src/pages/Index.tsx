75364afe7d973095eb8121221c87a2822c3f35d6
Akhilesh2006s <155376342+Akhilesh2006s@users.noreply.github.com>
2025-06-15 10:36:48 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -32,9 +32,9 @@ const words = `95% of customer calls at a vehicle dealership are never reviewed.
 
 // Card data with natural sizing
 const cardData = [
-  { image: "/1.jpg" },
-  { image: "./public/2.jpg" },
-  { image: "src/pages/3.jpg" },
+  { image: "./1.jpg" },
+  { image: "./public/1.jpg" },
+  { image: "public/1.jpg" },
   { image: "src/pages/4.jpg" },
   { image: "src/pages/5.jpg" },
   { image: "src/pages/6.jpg" },
